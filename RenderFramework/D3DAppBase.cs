8add8b240c5dd4f591807936d637cce3d543ea47
Taehyung Kim <gaiaengine@gmail.com>
2018-07-12 17:18:41 +0900

    Add files via upload

---
+++ RenderFramework/D3DAppBase.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+
+using Device1 = SharpDX.Direct3D11.Device1;
+using Buffer  = SharpDX.Direct3D11.Buffer;
+
+namespace RenderFramework
+{
+    public abstract class D3DAppBase : SharpDX.Component
+    {
+        MyDeviceManager           _deviceManager;
+
+        // There presentation capabilities consist of specifying dirty rectangles and scroll rectangle to optimize presentation 
+        SharpDX.DXGI.SwapChain1 _swapChain1;
+
+        SharpDX.Direct3D11.RenderTargetView _renderTargetView;
+        SharpDX.Direct3D11.DepthStencilView _depthStencilView;
+        SharpDX.Direct3D11.Texture2D        _backBuffer;
+        SharpDX.Direct3D11.Texture2D        _depthBuffer;
+
+        // A bitmap can be used as a surface for SharpDX.Direct2D1.DeviceContext
+        // or mapped into system memory, and can contain additional color context information
+        SharpDX.Direct2D1.Bitmap1           _bitmapTarget;
+        
+        /// <summary>
+        /// Gets the configured bounds of the control used to render to 
+        /// </summary>
+        public SharpDX.Rectangle Bounds { get; protected set; }
+
+        /// <summary>
+        /// Gets the configured bounds of the control used to render to 
+        /// </summary>
+        public abstract SharpDX.Rectangle CurrentBounds { get; }
+
+        /// <summary>
+        /// Gets the <see cref="MyDeviceManager"/> attached to this instance
+        /// </summary>
+        public MyDeviceManager DeviceManager { get { return _deviceManager; } }
+
+        /// <summary>
+        /// Gets the <see cref="SharpDX.DXGI.SwapChain1"/> created by this instance
+        /// </summary>
+        public SharpDX.DXGI.SwapChain1 SwapChain1 { get { return _swapChain1; } }
+
+        /// <summary>
+        /// Provides access to the list of available display modes.
+        /// </summary>
+        public SharpDX.DXGI.ModeDescription[] DisplayModeList { get; private set; }
+
+        /// <summary>
+        /// Gets/sets whether the swap chain will wait for the 
+        /// next vertical sync before presenting.
+        /// </summary>
+        /// <remarks>
+        /// Changes the behavior of the <see cref="D3DAppBase.Present"/>method
+        /// </remarks>
+        public bool VSync { get; set; }
+
+        /// <summary>
+        /// Height of the swap chain buffers
+        /// </summary>
+        public virtual int Width
+        {
+            get
+            {
+                return (int)(Bounds.Width * DeviceManager.Dpi / 96.0);
+            }
+        }
+
+        /// <summary>
+        /// Height of the swap chain buffers
+        /// </summary>
+        public virtual int Height
+        {
+            get
+            {
+                return (int)(Bounds.Height * DeviceManager.Dpi / 96.0);
+            }
+        }
+
+        // get; set; -> auto implemented property
+        protected SharpDX.ViewportF Viewport { get; set; }
+        protected SharpDX.Rectangle RenderTargetBounds { get; set; }
+        protected SharpDX.Size2     RenderTargetSize { get { return new SharpDX.Size2(RenderTargetBounds.Width, RenderTargetBounds.Height); } }
+
+        public SharpDX.Direct3D11.RenderTargetView RenderTargetView
+        {
+            get
+            {
+                return _renderTargetView;
+            }
+
+            protected set
+            {
+                if(_renderTargetView != value)
+                {
+                    RemoveAndDispose(ref _renderTargetView);
+                    _renderTargetView = value;
+                }
+            }
+        }
+
+        /// <summaray>
+        /// Gets or sets the back buffer used by this app.
+        /// </summaray>
+        public SharpDX.Direct3D11.Texture2D BackBuffer
+        {
+            get
+            {
+                return _backBuffer;
+            }
+
+            protected set
+            {
+                if(_backBuffer != value)
+                {
+                    RemoveAndDispose(ref _backBuffer);
+                    _backBuffer = value;
+                }
+            }
+        }
+
+        /// <summaray> 
+        /// Gets or sets the depthbuffer used by this app
+        /// </summaray>
+        public SharpDX.Direct3D11.Texture2D DepthBuffer
+        {
+            get
+            {
+                return _depthBuffer;
+            }
+
+            protected set
+            {
+                if(_depthBuffer != value)
+                {
+                    RemoveAndDispose(ref _depthBuffer);
+                    _depthBuffer = value;
+                }
+            }
+        }
+
+        /// <summaray>
+        /// Gets the Direct3D DepthStencilView used by this app
+        /// </summaray>
+        public SharpDX.Direct3D11.DepthStencilView DepthStencilView
+        {
+            get
+            {
+                return _depthStencilView;
+            }
+            protected set
+            {
+                if(_depthStencilView != value)
+                {
+                    RemoveAndDispose(ref _depthStencilView);
+                    _depthStencilView = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the Direct2D RenderTarget used by this app.
+        /// </summary>
+        public SharpDX.Direct2D1.Bitmap1 BitmapTarget2D
+        {
+            get { return _bitmapTarget; }
+            protected set
+            {
+                if (_bitmapTarget != value)
+                {
+                    RemoveAndDispose(ref _bitmapTarget);
+                    _bitmapTarget = value;
+                }
+            }
+        }
+
+        /// <summaray>
+        /// Default Constructor
+        /// </summaray>
+        public D3DAppBase()
+        {
+            // Create our device manager instance.
+            // This encapsulates the creation of Direct3D and Direct2D devices
+            _deviceManager = ToDispose(new MyDeviceManager());
+
+            // If the device needs to be reinitialized, make sure we
+            // are able to recreate our device dependent resources.
+            //DeviceManager.OnInitialize += 
+        }
+
+        ///<summaray>
+        /// Trigger the OnSizeChanged event if the width and height of the <see cref="CurrentBounds"/>
+        /// differs to the last call to SizeChanged
+        /// </summaray>
+        protected void SizeChanged(bool force = false)
+        {
+            var newBounds = CurrentBounds;
+
+            // Ignore invalid sizes, 
+        }
+
+        /// <summary>
+        /// Create device dependent resources
+        /// </summary>
+        /// <param name="deviceManager"></param>
+        protected virtual void CreateDeviceDependentResources(MyDeviceManager deviceManager)
+        {
+            if(_swapChain1 != null)
+            {
+                // Release the swap chain
+                RemoveAndDispose(ref _swapChain1);
+
+                // Force reinitialize size dependent resources
+                SizeChanged(true);
+            }
+        }
+
+        /// <summaray>
+        /// Create size dependent resoruces, in this case the swap chain and render targets
+        /// </summaray>
+        /// <param name = "app"></param>
+        protected virtual void CreateSizeDependentResources(D3DAppBase app)
+        {
+        
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ RenderFramework/MyDeviceManager.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+
+namespace RenderFramework
+{
+    public class MyDeviceManager : SharpDX.Component
+    {
+        // Declare Direct3D objects
+        protected SharpDX.Direct3D11.Device1        d3dDevice;
+        protected SharpDX.Direct3D11.DeviceContext1 d3dContext;
+        protected float _Dpi;
+
+        // Declare Direct2D objects
+        protected SharpDX.Direct2D1.Device        d2dDevice;
+        protected SharpDX.Direct2D1.DeviceContext d2dContext;
+        protected SharpDX.Direct2D1.Factory1 d2dFactory;
+
+        // Declare DirectWrite & Windows Imaging Component Objects
+        protected SharpDX.DirectWrite.Factory dwriteFactory;
+        protected SharpDX.WIC.ImagingFactory2 wicFactory;
+
+        /// <summary>
+        /// The list of feature level to accept
+        /// </summary>
+        public SharpDX.Direct3D.FeatureLevel[] D3DFeatureLevel = new FeatureLevel[]
+        {
+            FeatureLevel.Level_11_1,
+            FeatureLevel.Level_11_0,
+        };
+
+        /// <summary>
+        /// Gets the Direct3D11(ver 11.1) device
+        /// </summary>
+        public SharpDX.Direct3D11.Device1 Direct3DDevice { get { return d3dDevice; } }
+
+        /// <summary>
+        /// Gets the Direct3D11 immediate context
+        /// </summary>
+        public SharpDX.Direct3D11.DeviceContext1 Direct3DContext { get { return d3dContext; } }
+
+        /// <summary>
+        /// Gets the Direct2D device
+        /// </summary>
+        public SharpDX.Direct2D1.Device Direct2DDevice { get { return d2dDevice; } }
+
+        /// <summary>
+        /// Gets the Direct2D context
+        /// </summary>
+        public SharpDX.Direct2D1.DeviceContext Direct2DContext { get { return d2dContext; } }
+        
+        /// <summary>
+        /// Gets the Direct2D factory
+        /// </summary>
+        public SharpDX.Direct2D1.Factory1 Direct2DFactory { get { return d2dFactory; } }
+
+        /// <summary>
+        /// Gets the DirectWrite factory
+        /// </summary>
+        public SharpDX.DirectWrite.Factory DirectWriteFactory { get { return dwriteFactory; } }
+
+        /// <summary>
+        /// Gets the Windows Imageing Component factory
+        /// </summary>
+        public SharpDX.WIC.ImagingFactory2 WICFactory { get { return wicFactory; } }
+
+        /// <summary>
+        /// Gets/Sets the DPI
+        /// </summary>
+        /// <remarks>
+        /// This method will fire the event <see cref="OnDpiChanged"/>
+        /// if the DPI is modified
+        /// </remarks>
+        public virtual float Dpi
+        {
+            get { return _Dpi; }
+            set
+            {
+                if(_Dpi != value)
+                {
+                    _Dpi = value;
+                    d2dContext.DotsPerInch = new SharpDX.Size2F(_Dpi, _Dpi);
+
+                    if(OnDpiChanged != null)
+                    {
+                        OnDpiChanged(this);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This event is fired when the MyDeviceManager is 
+        /// initialized by the <see cref="Initialize"/> method.
+        /// </summary>
+        public event Action<MyDeviceManager> OnInitialize;
+
+        /// <summary>
+        /// This event is fired when the <see cref="Dpi"/> is called.
+        /// </summary>
+        public event Action<MyDeviceManager> OnDpiChanged;
+
+        /// <summary>
+        /// Initialize this instance. 
+        /// </summary>
+        /// <param name="window"> Window to receive the rendering </param>
+        public virtual void Initialize(float dpi = 96.0f)
+        {
+            CreateInstance();
+
+            if (OnInitialize != null)
+            {
+                OnInitialize(this);
+            }
+
+            Dpi = dpi;
+        }
+            
+        /// <summary>
+        /// Create device manager objects
+        /// </summary>
+        /// <remarks>
+        /// This method is called at the initialization of this instance.
+        /// </remarks>
+        protected virtual void CreateInstance()
+        {
+            // Dispose previous references and set to null
+            RemoveAndDispose(ref d3dDevice);
+            RemoveAndDispose(ref d3dContext);
+            RemoveAndDispose(ref d2dDevice);
+            RemoveAndDispose(ref d2dContext);
+            RemoveAndDispose(ref d2dFactory);
+            RemoveAndDispose(ref dwriteFactory);
+            RemoveAndDispose(ref wicFactory);
+
+
+            #region Create Direct3D 11.1 device and retrieve device context
+
+            // BGRA performs better especially with Direct2D software render targets
+            var creationFlags = SharpDX.Direct3D11.DeviceCreationFlags.BgraSupport;
+#if DEBUG
+            // Enble D3D device debug layer
+            creationFlags |= SharpDX.Direct3D11.DeviceCreationFlags.Debug;
+#endif
+            // Retrieve the Direct3D 11.1 device and device context
+            using (var device = new SharpDX.Direct3D11.Device(DriverType.Hardware,
+                                                             creationFlags,
+                                                             D3DFeatureLevel))
+            {
+                d3dDevice = ToDispose(device.QueryInterface<Device1>());
+            }
+
+            // Get Direct3D 11.1 immediate context
+            d3dContext = ToDispose(d3dDevice.ImmediateContext.QueryInterface<DeviceContext1>());
+            #endregion
+
+            #region Create Direct2D device and context
+
+#if DEBUG
+            var debugLevel = SharpDX.Direct2D1.DebugLevel.Information;
+#endif
+            // Allocate new references
+            d2dFactory    = ToDispose(new SharpDX.Direct2D1.Factory1(SharpDX.Direct2D1.FactoryType.SingleThreaded, debugLevel));
+            dwriteFactory = ToDispose(new SharpDX.DirectWrite.Factory(SharpDX.DirectWrite.FactoryType.Shared));
+            wicFactory    = ToDispose(new SharpDX.WIC.ImagingFactory2());
+
+            // Create Direct2D device
+            using (var dxgiDevice = d3dDevice.QueryInterface<SharpDX.DXGI.Device>())
+            {
+                d2dDevice = ToDispose(new SharpDX.Direct2D1.Device(d2dFactory, dxgiDevice));
+            }
+
+            // Create Direct2D context
+            d2dContext = ToDispose(new SharpDX.Direct2D1.DeviceContext(d2dDevice, 
+                                                                       SharpDX.Direct2D1.DeviceContextOptions.None));
+
+            #endregion
+        }
+    }
+}
